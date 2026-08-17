00c5d6ebcda3e9ae74dcbf6d3de86d1ef8904cc4
Hirohito0521 <tachanhirohito@gmail.com>
2026-08-17 13:17:47 +0900

    Add files via upload

---
+++ T-pocket34-akdk/qmk_firmware/keyboards/t_pocket34/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x04, 0xec, 0xa9, 0xe7, 0xe5, 0x77, 0x0e, 0x7b}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
